50e220f64a6192745485f34232cfdb06bbc324df
Siddharth Tiwari <95074087+llSiddharthll@users.noreply.github.com>
2024-01-07 18:26:04 +0000

    new

---
+++ index.js
@@ -0,0 +1,10 @@
+$(document).ready(
+    function(){
+        var typed = new Typed(".typing", {
+            strings: ["Django Developer","Fresher","Machine Learning Developer", "Freelancer","Open Source Contributer"],
+            typeSpeed: 100,
+            backSpeed: 60,
+            loop: true
+        });
+    }
+);
